a7fb1cc86cb6ee6e9025c8323623cde1b5a0c30f
Vladas (Vlad) <vladaskidanovas@gmail.com>
2021-07-16 21:45:53 +0100

    lets goooo

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require("postcss-color-function", {preserveCustomProps: true})
+])
+.browserSync('portfolio.loc');
