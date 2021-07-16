8539e6913b825b4dbf409819578ff6d167258448
Vladas (Vlad) <vladaskidanovas@gmail.com>
2021-07-16 20:25:33 +0100

    Update solution.bat

--- solution.bat
+++ solution.bat
@@ -5,17 +5,38 @@ CLS
 set DIR=e:
 cd %DIR%\WEBDEV\Homestead
 
-CHOICE /N /C:12 /M "Vagrant (1 - UP, 2 - HALT)"%1
+CHOICE /N /C:12345 /M "Vagrant (1 - UP, 2 - HALT, 3- RELOAD, 4 - PROVISON, 5 - SSH )"%1
+IF ERRORLEVEL ==5 GOTO SSHTUNEL
+IF ERRORLEVEL ==4 GOTO PROVISION
+IF ERRORLEVEL ==3 GOTO RELOAD
 IF ERRORLEVEL ==2 GOTO HALT
 IF ERRORLEVEL ==1 GOTO UP
 GOTO END
+
 :HALT
 ECHO Vagrant is halting...
 vagrant halt
 GOTO END
+
 :UP
 ECHO Launching VAGRANT...
 vagrant up
 :END
+
+:RELOAD
+ECHO RELOADING...
+vagrant reload
+:END
+
+:PROVISON
+ECHO PROVISION...
+vagrant provison
+:END
+
+:SSHTUNEL
+ECHO CONNECTING THROUGH SSH
+vagrant ssh
+:END
+
 pause
 
