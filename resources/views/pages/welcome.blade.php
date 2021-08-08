8f4baf9486c5886addeb7a8e6729adeef921e78b
Vladas (Vlad) <vladaskidanovas@gmail.com>
2021-08-08 19:38:34 +0100

    changes

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,12 +5,12 @@
         <div class="brackets">
             <h2>{</h2><h1>}</h1>
         </div>
-        {{-- <div class="lines">
+        <div class="lines">
             <div class="line one"></div>
             <div class="line two"></div>
             <div class="line three"></div>
             <div class="line four"></div>
-        </div> --}}
+        </div>
         <div class="simple-about">
             Hi
         </div>
