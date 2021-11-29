0950cf9cb10592eef096cbf56bcf4a876a506961
Vlad <vladaskidanovas@gmail.com>
2021-11-29 12:14:19 +0000

    minor changes

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,24 +7,31 @@
                 <div class="row">
                     <div class="col-md-6 col-sm-0">
                         <h1 class="card-title">Want to get in touch?</h1>
-                        Also you can contact me using those:
-                        <div class="row">
-                            <div class="col">
-                                <a class="fs-1" href="https://github.com/vladaskidanovas" target="_blank">
-                                    <ion-icon name="logo-github"></ion-icon>
-                                </a>
-                                <a class="fs-1" href="https://www.linkedin.com/in/vladislovas-kidanovas/"
-                                    target="_blank">
-                                    <ion-icon name="logo-linkedin"></ion-icon>
-                                </a>
-                            </div>
-                        </div>
                     </div>
                     <div class="col-md-6 col-sm-12">
 
                         @if (session('status'))
+                            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path
+                                          d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                </symbol>
+                                <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path
+                                          d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                                </symbol>
+                                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path
+                                          d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </symbol>
+                            </svg>
+
                             <div class="col">
-                                <div class="alert alert-success">
+                                <div class="alert alert-primary">
+                                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img"
+                                         aria-label="Success:">
+                                        <use xlink:href="#check-circle-fill" />
+                                    </svg>
                                     {{ session('status') }}
                                 </div>
                             </div>
@@ -32,11 +39,12 @@
 
                             <form name="send_message" id="send_message" method="post" action="{{ url('send_message') }}">
                                 @csrf
+
                                 <div class="mb-3">
                                     <label for="formControlName" class="form-label">Name</label>
                                     <input type="text"
-                                        class="form-control {{ $errors->first('name') ? 'is-invalid' : '' }}"
-                                        id="formControlName" name="name" placeholder="Alan" required>
+                                           class="form-control {{ $errors->first('name') ? 'is-invalid' : '' }}"
+                                           id="formControlName" name="name" placeholder="Alan" required>
                                     <div id="validationName" class="invalid-feedback">
                                         {{ $errors->first('name') }}
                                     </div>
@@ -44,17 +52,17 @@
                                 <div class="mb-3">
                                     <label for="formControlEmail" class="form-label">Email address</label>
                                     <input type="email"
-                                        class="form-control {{ $errors->first('email_address') ? 'is-invalid' : '' }}"
-                                        id="formControlEmail" name="email_address" placeholder="name@example.com" required>
+                                           class="form-control {{ $errors->first('email_address') ? 'is-invalid' : '' }}"
+                                           id="formControlEmail" name="email_address" placeholder="name@example.com"
+                                           required>
                                     <div id="validationEmail" class="invalid-feedback">
                                         {{ $errors->first('email_address') }}
                                     </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="formControlMessageText" class="form-label">Message</label>
-                                    <textarea
-                                        class="form-control {{ $errors->first('message_text') ? 'is-invalid' : '' }} "
-                                        id="formControlMessageText" name="message_text" rows="9" required></textarea>
+                                    <textarea class="form-control {{ $errors->first('message_text') ? 'is-invalid' : '' }} "
+                                              id="formControlMessageText" name="message_text" rows="9" required></textarea>
                                     <div id="validationMessage" class="invalid-feedback">
                                         {{ $errors->first('message_text') }}
                                     </div>
