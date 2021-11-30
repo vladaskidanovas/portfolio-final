6fe8834cf1a9c24c4a46bbf9d41d4c62e761710c
Vlad <vladaskidanovas@gmail.com>
2021-11-30 20:29:01 +0000

    changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,16 +11,16 @@ class App {
             this.CChangeTheme(e);
         });
 
-        // document.getElementById('menuToggle').addEventListener('click', ()=>{
-        //     var x = document.getElementById("navbarNav");
-        //     if (x.classList.contains("show")) {
-        //         x.classList.add("hidden")
-        //         x.classList.remove("show")
-        //     } else {
-        //         x.classList.add("show")
-        //         x.classList.remove("hidden")
-        //     }
-        // });
+        document.getElementById('menuToggle').addEventListener('click', ()=>{
+            var x = document.getElementById("navbarNav");
+            if (x.classList.contains("show")) {
+                x.classList.add("hidden")
+                x.classList.remove("show")
+            } else {
+                x.classList.add("show")
+                x.classList.remove("hidden")
+            }
+        });
 
         document.addEventListener("mousemove", (e) => {
             this.WPBackgroundParalax(e);
