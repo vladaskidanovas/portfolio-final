4c23787e59779d4dae7f51061c10f6187c4cc3ff
Vlad <vladaskidanovas@gmail.com>
2021-11-30 18:17:37 +0000

    minor changes

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -9,7 +9,7 @@
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
               </svg>
         </button>
-        <div class="navbar-collapse collapse text-end hidden" id="navbarNav">
+        <div class="navbar-collapse text-end hidden" id="navbarNav">
             <ul class="navbar-nav ms-auto">
 
                 <li class="nav-item">
